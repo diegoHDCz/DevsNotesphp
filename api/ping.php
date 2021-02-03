5712b64de6730ca6099f026903040b4cf215d5d9
Diego Czajka <diego.czajka@gmail.com>
2021-02-03 10:21:06 -0300

    estrutura CORS

--- api/ping.php
+++ api/ping.php
@@ -1,8 +1,8 @@
 <?php
-
 require('../config.php');
 
 
 $array['pong'] = true;
+
 
 require('../return.php');
--- return.php
+++ return.php
@@ -2,7 +2,6 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Content-type: application/json");
+header("Content-Type: application/json");
 echo json_encode($array);
 exit;
-
